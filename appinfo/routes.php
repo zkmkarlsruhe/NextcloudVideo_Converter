0e04381e2672701ba423ccf4ce5beb32b852f7da
GityUpNow <kim-cheng@gmx.de>
2019-11-14 17:34:01 +0100

    Removed standard comment from routes
    Since it's serves no purpose here and seems to contain outdated
    instructions.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Create your routes in here. The name is the lowercase name of the controller
- * without the controller part, the stuff after the hash is the method.
- * e.g. page#index -> OCA\Extract\Controller\PageController->index()
- *
- * The controller class has to be registered in the application.php file since
- * it's instantiated in there
- */
 return [
     'resources' => [
         'preset' => ['url' => '/presets'],
@@ -17,5 +9,5 @@ return [
             'url'  => 'ajax/convertHere.php',
             'verb' => 'POST'
         ],
-    ]
+    ],
 ];
