038d4471d64bfa0cf42fef870b9c6f59c66c4fa5
GityUpNow <kim-cheng@gmx.de>
2019-11-22 11:29:30 +0100

    Replaced resources with normal routes
    To be less ambiguous.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,13 +1,35 @@
 <?php
 return [
-    'resources' => [
-        'preset' => ['url' => '/presets'],
-    ],
     'routes' => [
         [
             'name' => 'conversion#convertHere',
             'url'  => 'ajax/convertHere.php',
             'verb' => 'POST'
-        ],
+		],
+		[
+			'name' => 'preset#index',
+			'url'  => '/preset',
+			'verb' => 'GET'
+		],
+		[
+			'name' => 'preset#get',
+			'url'  => '/preset/{id}',
+			'verb' => 'GET'
+		],
+		[
+			'name' => 'preset#create',
+			'url'  => '/preset',
+			'verb' => 'POST'
+		],
+		[
+			'name' => 'preset#update',
+			'url'  => '/preset/{id}',
+			'verb' => 'PUT'
+		],
+		[
+			'name' => 'preset#remove',
+			'url'  => "/preset/{id}",
+			'verb' => 'DELETE'
+		],
     ],
 ];
