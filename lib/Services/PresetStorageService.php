8d1ce571383552c1a129a500067eed30817f6c7d
GityUpNow <kim-cheng@gmx.de>
2019-11-26 00:54:39 +0100

    Wrote preliminary code to load config
    Still depends on a unwritten PresetElement-Class tho, so it can't be
    finished yet.

--- lib/Services/PresetStorageService.php
+++ lib/Services/PresetStorageService.php
@@ -2,15 +2,41 @@
 namespace OCA\Video_Converter\Services;
 
 use OCP\IConfig;
+use OCP\ILogger;
 
 class PresetStorageService {
 	private $appName;
 	private $config;
+	private $logger;
+	private $presets;
 
-	public function __construct(string $appName, IConfig $config, string $userId) {
+	private const configKey = 'config';
+
+	public function __construct(string $appName, IConfig $config, ILogger $logger, string $userId) {
 		$this->appName = $appName;
 		$this->config = $config;
+		$this->logger = $logger;
 		$this->userId = $userId;
+
+
+		if (!configExists()) {
+			init();
+		} else {
+			$presetJSON = $this->config->getUserValue($this->userId, $this->appName, $this->configKey);
+			$error = $this->loadPresets($presetJSON);
+
+			if ($error != JSON_ERROR_NONE) {
+				$msg = json_last_error_msg();
+
+				$this->logger->error('Invalid JSON read from config', array(
+					'appName' => $this->appName,
+					'userId' => $this->userId,
+					'JSON' => $presetJSON
+				));
+
+				$this->reset();
+			}
+		}
 	}
 
 	public function init() {
@@ -37,8 +63,21 @@ class PresetStorageService {
 		
 	}
 
+	/*
+	 * @returns JSON_ERROR-Code or 0 if successful
+	 */
+	private function loadPresets(string $JSON): int {
+		$presetsArr = json_decode($JSON, true);
+
+		if ($presetsArr == null) {
+			return json_last_error();
+		}
+
+		//TODO: Add Present-Element and assemble a list of them
+	}
+
 	private function configExists(): bool {
-		if ($this->config->getUserValue($this->$userId, $this->appName, 'config') == '') {
+		if ($this->config->getUserValue($this->$userId, $this->appName, $this->configKey) == '') {
 			return false;
 		}
 
