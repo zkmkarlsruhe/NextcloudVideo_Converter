6fa4ff5866b59f4c1b24c9b0f7b182e2a681bb7c
GityUpNow <kim-cheng@gmx.de>
2019-11-25 03:19:09 +0100

    Implemented constructor for the StorageService

--- lib/Services/PresetStorageService.php
+++ lib/Services/PresetStorageService.php
@@ -1,8 +1,17 @@
 <?php
 namespace OCA\Video_Converter\Services;
 
+use OCP\IConfig;
+
 class PresetStorageService {
+	private $appName;
+	private $config;
 	
+	public function __construct(string $appName, IConfig $config) {
+		$this->appName = $appName;
+		$this->config = $config;
+	}
+
 	public function init() {
 
 	}
