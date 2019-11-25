430df126a777feea4c4c374019dac2f1f70d1fe7
GityUpNow <kim-cheng@gmx.de>
2019-11-25 02:06:04 +0100

    Created function skeleton

--- lib/Services/PresetStorageService.php
+++ lib/Services/PresetStorageService.php
@@ -2,5 +2,25 @@
 namespace OCA\Video_Converter\Services;
 
 class PresetStorageService {
+	
+	public function init() {
+
+	}
+
+	public function get() {
+
+	}
+
+	public function set() {
+
+	}
+
+	public function remove() {
+
+	}
+
+	public function reset() {
+		
+	}
 
 }
