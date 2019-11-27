c6dad84c760ac1124f3d2c5bcf6a58a972e44055
GityUpNow <kim-cheng@gmx.de>
2019-11-27 07:48:03 +0100

    Set up remaining state in constructor
    I should maybe check for NotPermittedExceptions. On the other hand, this
    is our AppData, so there's no reason why this should even happen.

--- lib/Services/UserStorageService.php
+++ lib/Services/UserStorageService.php
@@ -1,12 +1,31 @@
 <?php
 namespace OCA\Video_Converter\Services;
 
+use OCP\ILogger;
+use OCP\Files\IAppData;
+use OCP\Files\NotFoundException;
+use OCP\Files\SimpleFS\ISimpleFile;
+use OCP\Files\SimpleFS\ISimpleFolder;
+
+//TODO: Catch RuntimeExceptions
+//TODO: (Maybe) handle NotPermittedException. Shouldn't apply
 class UserStorageService {
 	private $appName;
 	private $userId;
+	private $appData;
+	private $logger;
 
-	public function __construct(string $appName, string $userId) {
+	public function __construct(string $appName, IAppData $appData, ILogger $logger, string $userId) {
 		$this->appName = $appName;
+		$this->appData = $appData;
+		$this->logger = $logger;
 		$this->userId = $userId;
+
+		$folder = null;
+		try {
+			$folder = $this->appData->getFolder($this->userId);
+		} catch (NotFoundException $e) {
+			$folder = $this->appData->newFolder($this->$userId);
+		}
 	}
 }
