15de5f8ddad0ef737e78def6f35fe4003120f8cf
GityUpNow <kim-cheng@gmx.de>
2019-11-27 07:08:10 +0100

    Implemented UserStorageService-Constructor

--- lib/Services/UserStorageService.php
+++ lib/Services/UserStorageService.php
@@ -2,5 +2,11 @@
 namespace OCA\Video_Converter\Services;
 
 class UserStorageService {
-	
+	private $appName;
+	private $userId;
+
+	public function __construct(string $appName, string $userId) {
+		$this->appName = $appName;
+		$this->userId = $userId;
+	}
 }
