109fa5591eb214c83a1bc975c8df6a2b5dc935ab
GityUpNow <kim-cheng@gmx.de>
2020-01-06 10:15:31 +0100

    Documented types and formats in current preset.

--- lib/Entities/Preset.php
+++ lib/Entities/Preset.php
@@ -2,20 +2,40 @@
 namespace OCA\Video_Converter\Entities;
 
 class Preset implements JsonSerializable {
+	/** 
+	 * All properties with either a primitive number or a string 
+	 * follow the following format:
+	 * In case of primitive number: <absolute value>
+	 * In case of string: "<primitive number>x" The primitive number 
+	 * here is a forfactor, designed for dynamic downscaling etc. related
+	 * to editing batches of files in the same way.
+	 */
+	/** @var string */
 	protected $audioCodec;
+	/** @var integer */
 	protected $bitrate;
+	/** @var integer */
 	protected $audioChannels;
 
+	/** @var string */
 	protected $videoCodec;
+	/** @var float|string */
 	protected $framerate;
+	/** @var integer|string */
 	protected $resolutionX;
+	/** @var integer|string */
 	protected $resolutionY;
 
+	/** @var string */
 	protected $watermarkPath;
+	/** @var integer|string */
 	protected $watermarkX;
+	/** @var integer|string */
 	protected $watermarkY;
+	/** @var string */
 	protected $watermarkMode;
 
+	/** @var Array */
 	protected $additionalArgs;
 
 }
