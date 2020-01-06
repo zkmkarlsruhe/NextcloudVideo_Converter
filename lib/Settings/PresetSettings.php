c12302136feff86053c8f04c951a819ad1f54b37
GityUpNow <kim-cheng@gmx.de>
2020-01-06 10:35:31 +0100

    Annotated PresetSettings.php

--- lib/Settings/PresetSettings.php
+++ lib/Settings/PresetSettings.php
@@ -6,29 +6,45 @@ use OCP\IConfig;
 use OCP\Settings\ISettings;
 
 class PresetSettings implements ISettings {
+	/** @var IConfig */
 	private $config;
+	/** @var string */
 	private $appName;
+	/** @var string */
 	private $userId;
 
-	public function __construct(IConfig $config, $appName, $userId) {
+	/**
+	 * @param IConfig $config AppConfig-Instance
+	 * @param string $appName
+	 * @param string $userId Id of the user requesting the page.
+	 */
+	public function __construct(IConfig $config, string $appName, string $userId) {
 		$this->$config = $config;
 		$this->appName = $appName;
 		$this->userId = $userId;
 	}
 
 	/**
+	 * Returns the Template for the Settings-Page.
+	 * 
 	 * @return TemplateResponse
 	 */
 	public function getForm() {
 		return new TemplateResponse($this->appName, 'settings');
 	}
 
+	/**
+	 * Returns the (internal) name of the used section.
+	 * 
+	 * @return string
+	 */
 	public function getSection() {
 		return $this->appName;
 	}
 
 	/**
 	 * Place at the bottom of the settings tab.
+	 * 
 	 * @return int
 	 */
 	public function getPriority() {
